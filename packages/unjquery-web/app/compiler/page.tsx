ec07b3eccea0f954c1ffa87e4efb63e937e58f64
void-sta1ker <iamdilshodboy@gmail.com>
2024-05-21 17:44:23 +0500

    feat@web: add randomizer

--- packages/unjquery-web/app/compiler/page.tsx
+++ packages/unjquery-web/app/compiler/page.tsx
@@ -6,8 +6,6 @@ import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import js from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
 import darcula from "react-syntax-highlighter/dist/esm/styles/prism/darcula";
 
-SyntaxHighlighter.registerLanguage("javascript", js);
-
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-textmate";
@@ -16,17 +14,28 @@ import "ace-builds/src-noconflict/ext-language_tools";
 
 import useIsDarkMode from "@/hooks/use-is-dark-mode";
 
+SyntaxHighlighter.registerLanguage("javascript", js);
+
 export default function Compiler() {
   const [value, setValue] = useState("");
   const [result, setResult] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [examples, setExamples] = useState<string[]>([]);
 
   const isDarkMode = useIsDarkMode();
 
   const onChange = (value: string) => {
     setValue(value);
+  };
+
+  const onRandomize = () => {
+    if (examples.length === 0) {
+      return;
+    }
+
+    setValue(examples[Math.floor(Math.random() * examples.length)]);
   };
 
   const onCompile = () => {
@@ -82,6 +91,10 @@ export default function Compiler() {
     }, 2000);
   }, [copied]);
 
+  useEffect(() => {
+    fetchExamples(setExamples);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-16">
       <h1 className="text-2xl font-semibold">
@@ -118,7 +131,14 @@ export default function Compiler() {
           />
 
           <button
-            className="absolute right-4 bottom-4 text-white bg-[#2b2b2b] rounded-md px-3 py-1 hover:bg-[#454545] transition duration-100"
+            className="absolute right-4 top-4 text-white text-sm bg-red-500 rounded-md px-3 py-1 hover:bg-red-400 transition duration-100"
+            onClick={onRandomize}
+          >
+            random
+          </button>
+
+          <button
+            className="absolute right-4 bottom-4 text-white text-sm bg-[#2b2b2b] rounded-md px-3 py-1 hover:bg-[#454545] transition duration-100"
             onClick={onCompile}
           >
             compile
@@ -127,7 +147,7 @@ export default function Compiler() {
 
         <span>➔</span>
 
-        <div className="w-[500px] h-[500px] bg-[#2b2b2b] pl-[10px] overflow-hidden rounded-md">
+        <div className="w-[500px] h-[500px] bg-[#2b2b2b] overflow-hidden rounded-md">
           <div className="w-full h-full overflow-auto relative">
             {result === "" ? (
               <p className="text-[#a9b7c6] p-[10px] text-sm">
@@ -136,14 +156,20 @@ export default function Compiler() {
                 {!loading && error ? error : null}
               </p>
             ) : (
-              <SyntaxHighlighter language="javascript" style={darcula}>
+              <SyntaxHighlighter
+                language="javascript"
+                style={darcula}
+                customStyle={{
+                  fontSize: "14px",
+                }}
+              >
                 {result}
               </SyntaxHighlighter>
             )}
 
             {result && (
               <button
-                className="absolute right-4 bottom-4 text-white bg-blue-600 rounded-md px-3 py-1 hover:bg-blue-500 transition duration-100"
+                className="absolute right-4 bottom-4 text-white text-sm bg-blue-600 rounded-md px-3 py-1 hover:bg-blue-500 transition duration-100"
                 onClick={onCopy}
               >
                 {copied ? "✔" : "copy"}
@@ -154,4 +180,14 @@ export default function Compiler() {
       </div>
     </div>
   );
+}
+
+function fetchExamples(setter: React.Dispatch<React.SetStateAction<string[]>>) {
+  fetch("/api/examples")
+    .then((res) => res.json())
+    .then((data) => {
+      const examples = data.content.split("\n\n");
+
+      setter(examples);
+    });
 }
